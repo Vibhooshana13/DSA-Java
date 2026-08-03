7b147aed2923f9a1e9d8ac1047cea40c69e11ea5
Vibhooshana S <216117305+Vibhooshana13@users.noreply.github.com>
2026-08-03 22:15:22 +0530

    Day 15: Solved LeetCode #202 - Happy Number

---
+++ Math/HappyNumber.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+public class HappyNumber {
+
+    public static boolean isHappy(int n) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+
+            seen.add(n);
+
+            int sum = 0;
+
+            while (n > 0) {
+
+                int digit = n % 10;
+                sum = sum + (digit * digit);
+                n = n / 10;
+
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 19;
+
+        System.out.println(isHappy(n));
+
+    }
+}
