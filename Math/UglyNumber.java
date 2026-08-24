cd1ec27652fe585c170e31502203f37173f41c34
Vibhooshana S <216117305+Vibhooshana13@users.noreply.github.com>
2026-08-24 22:35:04 +0530

    Day 22: Solved LeetCode #263 - Ugly Number

---
+++ Math/UglyNumber.java
@@ -0,0 +1,32 @@
+public class UglyNumber {
+
+    public static boolean isUgly(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        boolean result = isUgly(n);
+
+        System.out.println(n + " is an ugly number: " + result);
+    }
+}
